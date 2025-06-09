49609d78bab4b61cc58084a6a04c61a5f7877ba1
Sahosting <studentapnar@gmail.com>
2025-06-09 23:30:02 +0600

    install.sh

--- install.sh
+++ install.sh
@@ -7,10 +7,10 @@ opkg update
 opkg install iptables iptables-mod-nat-extra redsocks
 
 #Then run this line
-service redsocks stop && mv /etc/redsocks.conf /etc/redsocks.conf.bkp && cd /etc && wget https://github.com/emonbhuiyan/BDIX-OpenWRT/raw/main/bdix.conf && mv /etc/init.d/redsocks /etc/init.d/redsocks.bkp && cd /etc/init.d && wget https://github.com/emonbhuiyan/BDIX-OpenWRT/raw/main/bdix && chmod +x /etc/init.d/bdix
+service redsocks stop && mv /etc/redsocks.conf /etc/redsocks.conf.bkp && cd /etc && wget https://github.com/Sahosting/BdixBypass/blob/main/bdix.conf && mv /etc/init.d/redsocks /etc/init.d/redsocks.bkp && cd /etc/init.d && wget https://github.com/Sahosting/BdixBypass/blob/main/bdix && chmod +x /etc/init.d/bdix
 
 cd /
 clear
 
 
-echo -e "Thanks for installing. Follow me for more updates: https://fb.me/emoncontact"
+echo - Socks 5 is Ready to Use...
